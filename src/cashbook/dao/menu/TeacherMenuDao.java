ff139a239bb08ffcfac62dbb89a8ce84db2f7f72
YAMAMOTO <dummy@example.com>
2024-06-06 09:44:14 +0900

    メソッドのコメントを修正

--- src/cashbook/dao/menu/TeacherMenuDao.java
+++ src/cashbook/dao/menu/TeacherMenuDao.java
@@ -8,7 +8,7 @@ public interface TeacherMenuDao {
 	/**
 	 * 講師メニュー画面用の一覧データを検索する
 	 * @param formMap フォーム項目
-	 * @return 個人マスタ一覧
+	 * @return 一覧データ
 	 */
 	public List<Map<String, String>> searchQuestion(Map<String, Object> formMap);
 
